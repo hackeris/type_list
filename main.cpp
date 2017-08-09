52ee147b7a607f7a7163ae1e11b34273c7c588a2
hackeris <hackeris@qq.com>
2017-08-09 19:36:20 +0800

    init project

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "type_list.h"
+
+int main() {
+
+    using types = make_type_list<int, char, bool, void>::result;
+
+    std::cout << "length of <char, bool, void>: " << length_of<types>::value << std::endl;
+    std::cout << "index of char: " << index_of<types, int>::value << std::endl;
+    std::cout << "index of bool: " << index_of<types, char>::value << std::endl;
+    std::cout << "index of void: " << index_of<types, bool>::value << std::endl;
+    std::cout << "index of int: " << index_of<types, long>::value << std::endl;
+
+    return 0;
+}
---
+++ type_list.h
@@ -0,0 +1,63 @@
+//
+// Created by rainm on 2017/8/9.
+//
+
+#ifndef TYPELIST_TYPE_LIST_H
+#define TYPELIST_TYPE_LIST_H
+
+struct empty_type_list {
+};
+
+template<typename T, typename U>
+struct type_list {
+    using head = T;
+    using tail = U;
+};
+
+template<typename...>
+struct make_type_list;
+
+template<>
+struct make_type_list<> {
+    using result = empty_type_list;
+};
+
+template<typename T, typename ...types>
+struct make_type_list<T, types...> {
+    using tail = typename make_type_list<types...>::result;
+    using result = type_list<T, tail>;
+};
+
+template<typename>
+struct length_of;
+
+template<>
+struct length_of<empty_type_list> {
+    static constexpr int value = 0;
+};
+
+template<typename head, typename tail>
+struct length_of<type_list<head, tail>> {
+    static constexpr int value = length_of<tail>::value + 1;
+};
+
+template<typename, typename>
+struct index_of;
+
+template<typename T>
+struct index_of<empty_type_list, T> {
+    static constexpr int value = -1;
+};
+
+template<typename T, typename tail>
+struct index_of<type_list<T, tail>, T> {
+    static constexpr int value = 0;
+};
+
+template<typename head, typename tail, typename T>
+struct index_of<type_list<head, tail>, T> {
+    using index_in_tail = index_of<tail, T>;
+    static constexpr int value = (index_in_tail::value == -1) ? -1 : index_in_tail::value + 1;
+};
+
+#endif //TYPELIST_TYPE_LIST_H
